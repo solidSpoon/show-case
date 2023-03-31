762992ab89948bef10a873c4445c7b20ac124273
solidSpoon <cedarrival@outlook.com>
2023-03-31 14:01:13 +0800

    init

---
+++ src/main/java/xyz/solidspoon/showCase/ShowCase.java
@@ -0,0 +1,13 @@
+package xyz.solidspoon.showCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowCase {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowCase.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/solidspoon/showCase/ShowCaseTests.java
@@ -0,0 +1,13 @@
+package xyz.solidspoon.showCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowCaseTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
